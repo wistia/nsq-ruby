f3636d73cd52a532b79d2d897ae0f7bd4eb40378
Robby Grossman <robby@freerobby.com>
2014-07-23 11:37:27 -0400

    Move setup/cleanup to before/after blocks.

--- spec/lib/nsq/consumer_spec.rb
+++ spec/lib/nsq/consumer_spec.rb
@@ -2,36 +2,29 @@ require_relative '../../spec_helper'
 require 'json'
 
 describe Nsq::Consumer do
-
   before do
     @cluster = NsqCluster.new(nsqd_count: 1)
     @cluster.block_until_running
     @nsqd = @cluster.nsqd.first
-  end
-
-  after do
-    consumer.terminate
-    @cluster.destroy
-  end
-
-  let(:topic) { 'some-topic' }
-
-  subject(:consumer) do
-    Nsq::Consumer.new(
-      topic: topic,
+    @topic = 'some-topic'
+    @consumer = Nsq::Consumer.new(
+      topic: @topic,
       channel: 'some-channel',
       host: @nsqd.host,
       port: @nsqd.tcp_port
     )
   end
+  after do
+    @consumer.terminate
+    @cluster.destroy
+  end
 
   describe '#messages' do
     it 'can pop off a message' do
-      @nsqd.pub(topic, 'some-message')
-      msg = consumer.messages.pop
+      @nsqd.pub(@topic, 'some-message')
+      msg = @consumer.messages.pop
       expect(msg.body).to eq('some-message')
       msg.finish
     end
   end
-
 end
--- spec/lib/nsq/producer_spec.rb
+++ spec/lib/nsq/producer_spec.rb
@@ -2,30 +2,24 @@ require_relative '../../spec_helper'
 require 'json'
 
 describe Nsq::Producer do
-
   before do
     @cluster = NsqCluster.new(nsqd_count: 1)
     @cluster.block_until_running
     @nsqd = @cluster.nsqd.first
-  end
-
-  after do
-    @cluster.destroy
-  end
-
-  let(:topic) { 'some-topic' }
-
-  subject(:producer) do
-    Nsq::Producer.new(
-      topic: topic,
+    @topic = 'some-topic'
+    @producer = Nsq::Producer.new(
+      topic: @topic,
       host: @nsqd.host,
       port: @nsqd.tcp_port
     )
   end
+  after do
+    @cluster.destroy
+  end
 
   def message_count
     topics_info = JSON.parse(@nsqd.stats.body)['data']['topics']
-    topic_info = topics_info.select{|t| t['topic_name'] == topic }.first
+    topic_info = topics_info.select{|t| t['topic_name'] == @topic }.first
     if topic_info
       topic_info['message_count']
     else
@@ -35,14 +29,13 @@ describe Nsq::Producer do
 
   describe '#write' do
     it 'can queue a message' do
-      producer.write('some-message')
+      @producer.write('some-message')
       expect(message_count).to eq(1)
     end
 
     it 'can queue multiple messages at once' do
-      producer.write(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+      @producer.write(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
       expect(message_count).to eq(10)
     end
   end
-
 end
