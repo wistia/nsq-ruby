17f4d3cfff3c9267a26dd2740fba1c457a1a3c7d
Brendan Schwartz <brendanschwartz@gmail.com>
2016-04-20 18:13:10 -0400

    Bumping to 1.6.0. Now with TLS support thanks to @alieander. w00t!

--- lib/version.rb
+++ lib/version.rb
@@ -1,8 +1,8 @@
 module Nsq
   module Version
     MAJOR = 1
-    MINOR = 5
-    PATCH = 1
+    MINOR = 6
+    PATCH = 0
     BUILD = nil
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
