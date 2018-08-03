2bbe78d6b30ab85d219eafd76c9868850d7c0e57
Soulou <leo@unbekandt.eu>
2018-08-03 17:40:38 +0200

    Add second example with throttling

--- examples/consumer-pause/main.rb
+++ examples/consumer-pause/main.rb
@@ -4,6 +4,9 @@ lib = File.expand_path("../../../lib", __FILE__)
 $:.unshift(lib)
 
 require 'nsq'
+require 'logger'
+
+logger = Logger.new STDOUT
 
 consumer = Nsq::Consumer.new(
   topic: 'test-pause',
@@ -16,7 +19,7 @@ consumer = Nsq::Consumer.new(
     loop do
       msg = consumer.pop
       puts "#{i} MSG #{msg.id}: #{msg.body}"
-      sleep 1
+      sleep 2
       msg.finish
     end
   }
@@ -28,14 +31,20 @@ producer = Nsq::Producer.new(
 
 producer.write Time.now
 producer.write Time.now
+producer.write Time.now
+producer.write Time.now
 
+logger.info "pausing consumer"
 consumer.pause
+logger.info "consumer paused"
 
 producer.write Time.now
 producer.write Time.now
 
 sleep 5
 
+logger.info "resuming consumer"
 consumer.resume
+logger.info "consumer resumed"
 
 sleep 5
