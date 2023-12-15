4bda9e1d6355c5408d6a5bb1e625be462adc9a64
Anders Chen <anders@wistia.com>
2023-12-15 09:24:58 +0700

    2.4.1: Fix NSQ connection version string number (#68)

--- lib/version.rb
+++ lib/version.rb
@@ -1,9 +1,5 @@
 module Nsq
   module Version
-    MAJOR = 2
-    MINOR = 3
-    PATCH = 1
-    BUILD = nil
-    STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
+    STRING = File.read(File.expand_path('../../VERSION', __FILE__)).strip
   end
 end
